12e95c9cd75db5eebd4dd61dea477baf56f1466b
Matthew Woodard <96838174+Newbie814@users.noreply.github.com>
2024-08-01 19:20:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,253 +1,171 @@
-const { JSDOM } = require("jsdom");
+const { JSDOM } = require('jsdom');
 
 const processNode = (node, marks = []) => {
-    if (node.nodeType === 3) { // Node.TEXT_NODE is 3
-        return {
-            type: "text",
-            text: node.nodeValue,
-            marks: marks,
-        };
-    }
-
-    let textNode = {
-        type: "text",
-        text: node.textContent,
-        marks: marks,
+  if (node.nodeType === 3) {
+    // Text node
+    return {
+      type: 'text',
+      text: node.textContent,
+      marks: [...marks],
     };
+  }
 
-    if (node.nodeName === "CODE") {
-        textNode.marks.push({ type: "code" });
-    } else {
-        if (node.nodeName === "STRONG" || node.nodeName === "B") {
-            textNode.marks.push({ type: "strong" });
-        }
-        if (node.nodeName === "DEL") {
-            textNode.marks.push({ type: "strike" });
-        }
-        if (node.nodeName === "EM" || node.nodeName === "I") {
-            textNode.marks.push({ type: "em" });
-        }
-        if (node.nodeName === "U") {
-            textNode.marks.push({ type: "underline" });
-        }
-        if (node.nodeName === "A") {
-            textNode.marks.push({
-                type: "link",
-                attrs: {
-                    href: node.getAttribute("href"),
-                    title: node.textContent,
-                },
-            });
-        }
-        if (node.nodeName === "BR") {
-            return { type: "text", text: "\n", marks: [] };
-        }
-    }
+  if (node.nodeName === 'BR') {
+    return {
+      type: 'hardBreak',
+    };
+  }
 
-    if (node.childNodes.length > 0) {
-        let childNodes = Array.from(node.childNodes)
-            .map((childNode) => processNode(childNode, textNode.marks))
-            .flat();
+  let textNode = {
+    type: 'text',
+    text: node.textContent,
+    marks: [...marks],
+  };
 
-        if (node.nodeName !== "BR") {
-            textNode = { ...textNode, content: childNodes };
+  switch (node.nodeName) {
+    case 'CODE':
+      textNode.marks.push({ type: 'code' });
+      break;
+    case 'STRONG':
+    case 'B':
+      textNode.marks.push({ type: 'strong' });
+      break;
+    case 'DEL':
+      textNode.marks.push({ type: 'strike' });
+      break;
+    case 'EM':
+    case 'I':
+      textNode.marks.push({ type: 'em' });
+      break;
+    case 'U':
+      textNode.marks.push({ type: 'underline' });
+      break;
+    case 'A':
+      textNode.marks.push({
+        type: 'link',
+        attrs: {
+          href: node.getAttribute('href'),
+          title: node.textContent,
+        },
+      });
+      break;
+  }
+
+  if (node.childNodes.length > 0) {
+    return Array.from(node.childNodes)
+      .map((childNode) => processNode(childNode, textNode.marks))
+      .flat();
+  }
+
+  return textNode;
+};
+
+const handleList = (node, listType) => {
+  const items = Array.from(node.children).map((li) => {
+    const liContent = Array.from(li.childNodes)
+      .map((childNode) => {
+        if (childNode.nodeName === 'UL' || childNode.nodeName === 'OL') {
+          return handleList(
+            childNode,
+            childNode.nodeName === 'UL' ? 'bulletList' : 'orderedList'
+          );
         }
-    }
+        return processNode(childNode, []);
+      })
+      .flat()
+      .filter(Boolean);
 
-    return textNode;
+    return {
+      type: 'listItem',
+      content: [
+        {
+          type: 'paragraph',
+          content: liContent.filter(
+            (item) => item.type !== 'bulletList' && item.type !== 'orderedList'
+          ),
+        },
+        ...liContent.filter(
+          (item) => item.type === 'bulletList' || item.type === 'orderedList'
+        ),
+      ],
+    };
+  });
+
+  return {
+    type: listType,
+    content: items,
+  };
 };
 
 const convertToADF = (htmlString) => {
-    const dom = new JSDOM(htmlString);
-    const doc = dom.window.document;
-    const adf = {
-        version: 1,
-        type: "doc",
-        content: [],
-    };
+  const dom = new JSDOM(htmlString);
+  const document = dom.window.document;
+  const adf = {
+    version: 1,
+    type: 'doc',
+    content: [],
+  };
 
-    const processChildNodes = (node) => {
-        const textNodes = Array.from(node.childNodes).map((childNode) =>
-            processNode(childNode, [])
-        );
+  document.body.childNodes.forEach((node) => {
+    const textNodes = Array.from(node.childNodes)
+      .map((childNode) => processNode(childNode, []))
+      .flat()
+      .filter(Boolean);
 
-        return textNodes.filter((textNode) => textNode.text.trim().length > 0 || textNode.text === "\n");
-    };
-
-    doc.body.childNodes.forEach((node) => {
-        if (node.nodeName === "P") {
-            adf.content.push({
-                type: "paragraph",
-                content: processChildNodes(node),
-            });
-        } else if (node.nodeName.startsWith("H")) {
-            const level = parseInt(node.nodeName.substring(1));
-            adf.content.push({
-                type: "heading",
-                attrs: { level: level },
-                content: processChildNodes(node),
-            });
-        } else if (node.nodeName === "UL") {
-            const items = Array.from(node.childNodes).map((li) => {
-                return {
-                    type: "listItem",
-                    content: [{ type: "paragraph", content: processChildNodes(li) }],
-                };
-            });
-            adf.content.push({ type: "bulletList", content: items });
-        } else if (node.nodeName === "OL") {
-            const items = Array.from(node.childNodes).map((li) => {
-                return {
-                    type: "listItem",
-                    content: [{ type: "paragraph", content: processChildNodes(li) }],
-                };
-            });
-            adf.content.push({ type: "orderedList", content: items });
-        } else if (node.nodeName === "BLOCKQUOTE") {
-            adf.content.push({
-                type: "blockquote",
-                content: [{ type: "paragraph", content: processChildNodes(node) }],
-            });
-        } else if (node.nodeName === "DL") {
-            const items = Array.from(node.childNodes).map((dt) => {
-                return {
-                    type: "listItem",
-                    content: [{ type: "paragraph", content: processChildNodes(dt) }],
-                };
-            });
-            adf.content.push({ type: "bulletList", content: items });
+    switch (node.nodeName) {
+      case 'P':
+        adf.content.push({
+          type: 'paragraph',
+          content: textNodes,
+        });
+        break;
+      case 'H1':
+      case 'H2':
+      case 'H3':
+      case 'H4':
+      case 'H5':
+      case 'H6':
+        const level = parseInt(node.nodeName.substring(1));
+        adf.content.push({
+          type: 'heading',
+          attrs: {
+            level: level,
+          },
+          content: textNodes,
+        });
+        break;
+      case 'UL':
+        adf.content.push(handleList(node, 'bulletList'));
+        break;
+      case 'OL':
+        adf.content.push(handleList(node, 'orderedList'));
+        break;
+      case 'BLOCKQUOTE':
+        adf.content.push({
+          type: 'blockquote',
+          content: [
+            {
+              type: 'paragraph',
+              content: textNodes,
+            },
+          ],
+        });
+        break;
+      default:
+        if (textNodes.length > 0) {
+          adf.content.push({
+            type: 'paragraph',
+            content: textNodes,
+          });
         }
-    });
+    }
+  });
 
-    return adf;
+  return adf;
 };
 
 module.exports = convertToADF;
 
-
-
-// const processNode = (node, marks = [])=> {
-//     let textNode = {
-//       type: "text",
-//       text: node.textContent,
-//       marks: marks,
-//     };
-  
-//     if (node.nodeName === "CODE") {
-//       textNode.marks.push({ type: "code" });
-//     } else {
-//       if (node.nodeName === "STRONG") {
-//         textNode.marks.push({ type: "strong" });
-//       }
-//       if (node.nodeName === "DEL") {
-//         textNode.marks.push({ type: "strike" });
-//       }
-//       if (node.nodeName === "EM") {
-//         textNode.marks.push({ type: "em" });
-//       }
-//       if (node.nodeName === "U") {
-//         textNode.marks.push({ type: "underline" });
-//       }
-//       if (node.nodeName === "A") {
-//         textNode.marks.push({
-//           type: "link",
-//           attrs: {
-//             href: node.getAttribute("href"),
-//             title: node.textContent,
-//           },
-//         });
-//       }
-//       if (node.nodeName === "BR") {
-//         textNode = { type: "text", text: "  ", marks: [] };
-//       }
-//       if (node.childNodes.length > 0) {
-//         Array.from(node.childNodes)
-//           .map((childNode) => processNode(childNode, textNode.marks))
-//           .flat();
-//       }
-//     }
-  
-//     return textNode;
-// };
-  
-// const convertToADF = (htmlString) =>{
-//     const parser = new DOMParser();
-//     const doc = parser.parseFromString(htmlString, "text/html");
-//     const adf = {
-//       version: 1,
-//       type: "doc",
-//       content: [],
-//     };
-  
-//     doc.body.childNodes.forEach((node) => {
-//       const textNodes = Array.from(node.childNodes).map((childNode) =>
-//         processNode(childNode, []),
-//       );
-  
-//       if (node.nodeName === "P") {
-//         adf.content.push({
-//           type: "paragraph",
-//           content: textNodes.filter(Boolean),
-//         });
-//       } else if (node.nodeName.startsWith("H")) {
-//         const level = parseInt(node.nodeName.substring(1));
-//         adf.content.push({
-//           type: "heading",
-//           attrs: {
-//             level: level,
-//           },
-//           content: textNodes.filter(Boolean),
-//         });
-//       } else if (node.nodeName === "UL") {
-//         const items = Array.from(node.getElementsByTagName("LI")).map((li) => {
-//           const liTextNodes = Array.from(li.childNodes).map((liChildNode) =>
-//             processNode(liChildNode, []),
-//           );
-//           return {
-//             type: "listItem",
-//             content: [
-//               { type: "paragraph", content: liTextNodes.filter(Boolean) },
-//             ],
-//           };
-//         });
-//         adf.content.push({
-//           type: "bulletList",
-//           content: items,
-//         });
-//       } else if (node.nodeName === "OL") {
-//         const items = Array.from(node.getElementsByTagName("LI")).map((li) => {
-//           const liTextNodes = Array.from(li.childNodes).map((liChildNode) =>
-//             processNode(liChildNode, []),
-//           );
-//           return {
-//             type: "listItem",
-//             content: [
-//               { type: "paragraph", content: liTextNodes.filter(Boolean) },
-//             ],
-//           };
-//         });
-//         adf.content.push({
-//           type: "orderedList",
-//           content: items,
-//         });
-//       } else if (node.nodeName === "BLOCKQUOTE") {
-//         const blockquoteTextNodes = Array.from(node.childNodes).map(
-//           (blockquoteChildNode) => processNode(blockquoteChildNode, []),
-//         );
-//         adf.content.push({
-//           type: "blockquote",
-//           content: [
-//             {
-//               type: "paragraph",
-//               content: blockquoteTextNodes.filter(Boolean),
-//             },
-//           ],
-//           // content: blockquoteTextNodes.filter(Boolean),
-//         });
-//       }
-//     });
-//     return adf;
-// };
 
 // module.exports = convertToADF;
 
